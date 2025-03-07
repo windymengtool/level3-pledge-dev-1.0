8e754fabc7c2ce29a52a0d4fd20e175bae0ce50f
gust <2414566866@qq.com>
2025-03-08 00:11:56 +0800

    20250308_001156

--- 001---参考资料/001---clone-repos.bat
+++ 001---参考资料/001---clone-repos.bat
@@ -1,7 +1,6 @@
 @echo off
 echo deleting old folders...
 
-:: 第一部分：删除现有仓库文件夹
 rmdir /s /q Pledge 2>nul
 rmdir /s /q pledge-backend 2>nul
 rmdir /s /q pledge-fe 2>nul
@@ -9,7 +8,6 @@ rmdir /s /q pledgev2 2>nul
 
 echo Cloning repositories...
 
-:: 第二部分：克隆仓库
 git clone git@github.com:ProjectsTask/Pledge.git
 git clone git@github.com:ProjectsTask/pledge-backend.git
 git clone git@github.com:ProjectsTask/pledge-fe.git
@@ -17,7 +15,6 @@ git clone git@github.com:ProjectsTask/pledgev2.git
 
 echo Cleaning .git folders...
 
-:: 第三部分：删除 .git 文件夹
 rmdir /s /q Pledge\.git
 rmdir /s /q pledge-backend\.git
 rmdir /s /q pledge-fe\.git
